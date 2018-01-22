67ca97cb4d266f4a8709d2a403dad9357cdda65d
Dmitry Chaplinsky <chaplinsky.dmitry@gmail.com>
2018-01-22 03:02:06 +0200

    Fixing sentry reporting

--- run.py
+++ run.py
@@ -3,15 +3,16 @@
 import sys
 from vulyk.app import app
 
+if app.config.get("SENTRY_DSN"):
+    from raven.contrib.flask import Sentry
+    sentry = Sentry(app, dsn=app.config["SENTRY_DSN"])
+
+
 if __name__ == '__main__':
     extra_files = None
 
     if app.debug:
         extra_files = app._base_files_to_watch + app._plugin_files_to_watch
-
-    if app.config.get("SENTRY_DSN"):
-        from raven.contrib.flask import Sentry
-        sentry = Sentry(app, dsn=app.config["SENTRY_DSN"])
 
     if len(sys.argv) > 1:
         app.run(sys.argv[1], extra_files=extra_files)
